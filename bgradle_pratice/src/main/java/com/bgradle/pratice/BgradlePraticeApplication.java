5612598445e02fe9b3b02b973b87bb274d91f188
BuildTools <unconfigured@null.spigotmc.org>
2022-07-29 15:07:14 +0900

    first commit

---
+++ bgradle_pratice/src/main/java/com/bgradle/pratice/BgradlePraticeApplication.java
@@ -0,0 +1,13 @@
+package com.bgradle.pratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgradlePraticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgradlePraticeApplication.class, args);
+	}
+
+}
---
+++ bgradle_pratice/src/test/java/com/bgradle/pratice/BgradlePraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgradle.pratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgradlePraticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
